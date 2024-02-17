d12c03a6f048b076d3dd83d82c069236f8d3ede4
Kalina Warner <kalinamosher7@gmail.com>
2024-02-16 20:17:30 -0700

    Testing put and delete

--- controllers/skills.js
+++ controllers/skills.js
@@ -20,16 +20,15 @@ const skillsMiddleware = {
 
   getOne: async (req, res, next) => {
     try {
-      const userId = new ObjectId(req.params.id);
+      const skillId = new ObjectId(req.params.id);
       const result = await mongodb
         .getDb()
         .db()
         .collection('skills')
-        .find({ _id: userId })
-        .toArray();
+        .findOne({ _id: skillId });
 
-      if (result.length > 0) {
-        res.json(result[0]);
+      if (result) {
+        res.json(result);
       } else {
         res.status(404).json({ message: 'Skill not found' });
       }
@@ -42,16 +41,57 @@ const skillsMiddleware = {
   create: async (req, res, next) => {
     try {
       const newSkill = req.body;
-      await mongodb
+      const result = await mongodb
         .getDb()
         .db()
         .collection('skills')
         .insertOne(newSkill);
-      res.status(201).json({ id: newSkill._id });
+      res.status(201).json({ id: result.insertedId });
     } catch (error) {
       console.error(error);
       res.status(500).send('Internal Server Error');
-    };
+    }
+  },
+
+  put: async (req, res, next) => {
+    try {
+      const skillId = new ObjectId(req.params.id);
+      const updatedSkill = req.body;
+      const result = await mongodb
+        .getDb()
+        .db()
+        .collection('skills')
+        .updateOne({ _id: skillId }, { $set: updatedSkill });
+
+      if (result.modifiedCount === 1) {
+        res.status(200).json({ message: 'Skill updated successfully' });
+      } else {
+        res.status(404).json({ message: 'Skill not found' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  },
+
+  delete: async (req, res, next) => {
+    try {
+      const skillId = new ObjectId(req.params.id);
+      const result = await mongodb
+        .getDb()
+        .db()
+        .collection('skills')
+        .deleteOne({ _id: skillId });
+
+      if (result.deletedCount === 1) {
+        res.status(200).json({ message: 'Skill deleted successfully' });
+      } else {
+        res.status(404).json({ message: 'Skill not found' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
   },
 };
 
--- controllers/spells.js
+++ controllers/spells.js
@@ -25,11 +25,10 @@ const spellsMiddleware = {
         .getDb()
         .db()
         .collection('spell_book')
-        .find({ _id: userId })
-        .toArray();
+        .findOne({ _id: userId });
 
-      if (result.length > 0) {
-        res.json(result[0]);
+      if (result) {
+        res.json(result);
       } else {
         res.status(404).json({ message: 'Spell not found' });
       }
@@ -42,16 +41,57 @@ const spellsMiddleware = {
   create: async (req, res, next) => {
     try {
       const newSpell = req.body;
-      await mongodb
+      const result = await mongodb
         .getDb()
         .db()
         .collection('spell_book')
         .insertOne(newSpell);
-      res.status(201).json({ id: newSpell._id });
+      res.status(201).json({ id: result.insertedId });
     } catch (error) {
       console.error(error);
       res.status(500).send('Internal Server Error');
-    };
+    }
+  },
+
+  put: async (req, res, next) => {
+    try {
+      const spellId = new ObjectId(req.params.id);
+      const updatedSpell = req.body;
+      const result = await mongodb
+        .getDb()
+        .db()
+        .collection('spell_book')
+        .updateOne({ _id: spellId }, { $set: updatedSpell });
+
+      if (result.modifiedCount === 1) {
+        res.status(200).json({ message: 'Spell updated successfully' });
+      } else {
+        res.status(404).json({ message: 'Spell not found' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  },
+
+  delete: async (req, res, next) => {
+    try {
+      const spellId = new ObjectId(req.params.id);
+      const result = await mongodb
+        .getDb()
+        .db()
+        .collection('spell_book')
+        .deleteOne({ _id: spellId });
+
+      if (result.deletedCount === 1) {
+        res.status(200).json({ message: 'Spell deleted successfully' });
+      } else {
+        res.status(404).json({ message: 'Spell not found' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
   },
 };
 
